8f3e7d1d354fcfe30fefff73e2b107bf68813a4d
j-scull <joe-scullion@hotmail.com>
2023-05-11 22:40:20 +0100

    Add errorHandlers

--- src/main/java/projects/sensor/api/OpenApiSpecLoader.java
+++ src/main/java/projects/sensor/api/OpenApiSpecLoader.java
@@ -1,12 +1,14 @@
 package projects.sensor.api;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.APIKeyHandler;
 import io.vertx.ext.web.openapi.RouterBuilder;
+import io.vertx.ext.web.openapi.RouterBuilderOptions;
 import io.vertx.ext.web.validation.RequestParameter;
 import io.vertx.ext.web.validation.RequestParameters;
 import io.vertx.ext.web.validation.ValidationHandler;
@@ -46,55 +48,103 @@ public class OpenApiSpecLoader {
 //                                    APIKeyHandler.create(authProvider)
 //                                            .header(config.getString("name")));
 
-                    routerBuilder.operation("logData").handler(routingContext -> {
-                        RequestParameters params = routingContext.get(ValidationHandler.REQUEST_CONTEXT_KEY);
-                        RequestParameter temperature = params.queryParameter("temperature");
-                        RequestParameter humidity = params.queryParameter("humidity");
-                        Date dateTime = new Date();
-                        logger.info("logData - temperature = {}, humidity = {}, time = {}", temperature, humidity, dateTime);
-                        routingContext.response()
-                                .setStatusCode(201)
-                                .setStatusMessage("OK")
-                                .end();
-                    });
+                    RouterBuilderOptions options = new RouterBuilderOptions();
+                    options.setOperationModelKey("operationModel");
+                    routerBuilder.setOptions(options);
 
-                    routerBuilder.operation("listDataPoints").handler(routingContext -> {
-                        RequestParameters params = routingContext.get(ValidationHandler.REQUEST_CONTEXT_KEY);
-                        RequestParameter sensorId = params.pathParameter("sensorId");
-                        RequestParameter year = params.queryParameter("year");
-                        RequestParameter month = params.queryParameter("month");
-                        RequestParameter date = params.queryParameter("date");
-                        logger.info("listDataPoints - sensorId = {}, year = {}, month = {}, date = {}", sensorId, year, month, date);
-                        routingContext.response()
-                                .setStatusCode(200)
-                                .setStatusMessage("OK")
-                                .end(getDataPoints().toBuffer());
-                    });
+                    routerBuilder.operation("logData")
+                            .handler(routingContext -> {
+                                RequestParameters params = routingContext.get(ValidationHandler.REQUEST_CONTEXT_KEY);
+                                RequestParameter temperature = params.queryParameter("temperature");
+                                RequestParameter humidity = params.queryParameter("humidity");
+                                Date dateTime = new Date();
+                                logger.info("logData - temperature = {}, humidity = {}, time = {}", temperature, humidity, dateTime);
+                                routingContext.response()
+                                        .setStatusCode(201)
+                                        .setStatusMessage("OK")
+                                        .end();
+                            }).failureHandler(routingContext -> {
+                                JsonObject operation = routingContext.get("operationModel");
+                                routingContext.response()
+                                        .setStatusCode(400)
+                                        .setStatusMessage("Bad Request")
+                                        .end();
+                                logger.info("OpenApiSpecLoader - {} error", operation.getString("operationId"));
+                            });
 
-                    routerBuilder.operation("dataPointsRange").handler(routingContext -> {
-                        RequestParameters params = routingContext.get(ValidationHandler.REQUEST_CONTEXT_KEY);
-                        RequestParameter sensorId = params.pathParameter("sensorId");
-                        RequestParameter start = params.queryParameter("start");
-                        RequestParameter stop = params.queryParameter("stop");
-                        logger.info("listDataPoints - sensorId = {}, start = {}, stop = {}", sensorId, start, stop);
+                    routerBuilder.operation("listDataPoints")
+                            .handler(routingContext -> {
+                                RequestParameters params = routingContext.get(ValidationHandler.REQUEST_CONTEXT_KEY);
+                                RequestParameter sensorId = params.pathParameter("sensorId");
+                                RequestParameter year = params.queryParameter("year");
+                                RequestParameter month = params.queryParameter("month");
+                                RequestParameter date = params.queryParameter("date");
+                                logger.info("listDataPoints - sensorId = {}, year = {}, month = {}, date = {}", sensorId, year, month, date);
+                                routingContext.response()
+                                        .setStatusCode(200)
+                                        .setStatusMessage("OK")
+                                        .end(getDataPoints().toBuffer());
+                            }).failureHandler(routingContext -> {
+                                JsonObject operation = routingContext.get("operationModel");
+                                routingContext.response()
+                                        .setStatusCode(400)
+                                        .setStatusMessage("Bad Request")
+                                        .end();
+                                logger.info("OpenApiSpecLoader - {} error", operation.getString("operationId"));
+                            });
 
-                        routingContext.response()
-                                .setStatusCode(200)
-                                .setStatusMessage("OK")
-                                .end(getDataPoints().toBuffer());
-                    });
+                    routerBuilder.operation("dataPointsRange")
+                            .handler(routingContext -> {
+                                RequestParameters params = routingContext.get(ValidationHandler.REQUEST_CONTEXT_KEY);
+                                RequestParameter sensorId = params.pathParameter("sensorId");
+                                RequestParameter start = params.queryParameter("start");
+                                RequestParameter stop = params.queryParameter("stop");
+                                logger.info("listDataPoints - sensorId = {}, start = {}, stop = {}", sensorId, start, stop);
 
-                    routerBuilder.operation("listSensors").handler(routingContext -> {
-                        RequestParameters  params = routingContext.get(ValidationHandler.REQUEST_CONTEXT_KEY);
-                        logger.info("listSensors - params = {}", params);
-                        routingContext.response()
-                                .setStatusCode(200)
-                                .setStatusMessage("OK")
-                                .end(getSensors().toBuffer());
-                    });
+                                routingContext.response()
+                                        .setStatusCode(200)
+                                        .setStatusMessage("OK")
+                                        .end(getDataPoints().toBuffer());
+                            }).failureHandler(routingContext -> {
+                                JsonObject operation = routingContext.get("operationModel");
+                                routingContext.response()
+                                        .setStatusCode(400)
+                                        .setStatusMessage("Bad Request")
+                                        .end();
+                                logger.info("OpenApiSpecLoader - {} error", operation.getString("operationId"));
+                            });
+
+                    routerBuilder.operation("listSensors")
+                            .handler(routingContext -> {
+                                RequestParameters  params = routingContext.get(ValidationHandler.REQUEST_CONTEXT_KEY);
+                                logger.info("listSensors - params = {}", params);
+                                routingContext.response()
+                                        .setStatusCode(200)
+                                        .setStatusMessage("OK")
+                                        .end(getSensors().toBuffer());
+                            }).failureHandler(routingContext -> {
+                                JsonObject operation = routingContext.get("operationModel");
+                                routingContext.response()
+                                        .setStatusCode(400)
+                                        .setStatusMessage("Bad Request")
+                                        .end();
+                                logger.info("OpenApiSpecLoader - {} error", operation.getString("operationId"));
+                            });
 
                     // Generate the router
                     Router router = routerBuilder.createRouter();
+
+                    router.errorHandler(404, routingContext -> {
+                        JsonObject errorObject = new JsonObject();
+                        errorObject.put("code", 404);
+                        errorObject.put("message",
+                                (routingContext.failure() != null) ? routingContext.failure().getMessage() : "Not Found"
+                        );
+                        routingContext.response()
+                                .setStatusCode(404)
+                                .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                                .end(errorObject.encode());
+                    });
 
                     // Start server instance
                     HttpServer server = vertx.createHttpServer(new HttpServerOptions()
